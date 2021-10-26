fa44302e697a978e0848e75a230eea81ec12e0e6
Francisco Daniel Martinez <franiciscodaniel97@gmail.com>
2021-10-26 15:56:26 -0500

    CSV sacar los datos a un excel

--- normal.py
+++ normal.py
@@ -6,8 +6,11 @@ from pandas.core.indexes.base import Index
 '''CON CSV donde sacare los datos par amandar el valor 1'''
 #Lectura del archivo CSV
 arch_csv = pd.read_csv('1.csv', sep =',')
-jsCSV = arch_csv[['Participants']]
-jsCSV.to_json('jsCSV.json')
+#jsCSV = arch_csv[['Participants']]
+datosCVS = arch_csv[['Participants']]
+datosCVS.to_excel('1.xlsx')
+'''esta lines convierte la consulta del CVS a json'''
+#jsCSV.to_json('jsCSV.json')
 #listaCSV = []
 #listaCSV.append(arch_csv[['Participants']])
 #print(listaCSV)
@@ -16,6 +19,8 @@ jsCSV.to_json('jsCSV.json')
 #print(arch_csv[['Participants']]
 
 '''CON XLSX es a donde voy a mandar el 1 cuando Participants sea igual entre B19;B66'''
-arch_excel = pd.read_excel('2.xlsx', sep = ',')
+arch_excel = pd.read_excel('2.xlsx')
+
+'''en esta linea se solicitam los datos que se requieren para validar los datos del CVS con el XLSX'''
 jsXLSX = arch_excel[['CONTROL DE ASISTENCIA']].iloc[17:66]
-#print(arch_excel[['CONTROL DE ASISTENCIA']].iloc[17:66])
+print(jsXLSX)
