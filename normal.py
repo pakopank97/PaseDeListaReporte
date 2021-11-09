67be3eddf5bf4152682f28c8f266f006faf15247
Francisco Daniel Martinez <franiciscodaniel97@gmail.com>
2021-11-08 20:03:26 -0600

    ahora solamente con openpyxl

--- normal.py
+++ normal.py
@@ -1,26 +1,40 @@
 from os import sep
 import pandas as pd
 import openpyxl
+from openpyxl import load_workbook
 from pandas.core.indexes.base import Index   
 
-'''CON CSV donde sacare los datos par amandar el valor 1'''
+'''CON CSV donde sacare los datos par amandar el valor 1
 #Lectura del archivo CSV
 arch_csv = pd.read_csv('1.csv', sep =',')
 #jsCSV = arch_csv[['Participants']]
 datosCVS = arch_csv[['Participants']]
+print(df.loc['Participants'])
+#print(type(datosCVS))
+for i in datosCVS:
+    print(f"DATO: {i}")
+    pass '''
+'''Ahora con openpyxl hacemos esto'''    
+arch_csv = pd.read_csv('1.csv', sep =',')
+datosCVS = arch_csv[['Participants']]
 datosCVS.to_excel('1.xlsx')
+wb = load_workbook (filename='1.xlsx')
+ws = wb.active
+ws['B']
+
+
+#datosCVS.to_excel('1.xlsx')
 '''esta lines convierte la consulta del CVS a json'''
 #jsCSV.to_json('jsCSV.json')
 #listaCSV = []
 #listaCSV.append(arch_csv[['Participants']])
 #print(listaCSV)
 #print(arch_csv[['Participants']])
-
 #print(arch_csv[['Participants']]
 
-'''CON XLSX es a donde voy a mandar el 1 cuando Participants sea igual entre B19;B66'''
+'''CON XLSX es a donde voy a mandar el 1 cuando Participants sea igual entre B19;B66
 arch_excel = pd.read_excel('2.xlsx')
 
-'''en esta linea se solicitam los datos que se requieren para validar los datos del CVS con el XLSX'''
+en esta linea se solicitam los datos que se requieren para validar los datos del CVS con el XLSX
 jsXLSX = arch_excel[['CONTROL DE ASISTENCIA']].iloc[17:66]
-print(jsXLSX)
+#print(jsXLSX) '''
